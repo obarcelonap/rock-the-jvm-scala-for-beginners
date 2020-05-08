1bc7eaa8a8f854dede83e4e0328e8f7e60d666d9
Oriol Barcelona <obarcelonap@gmail.com>
2020-05-08 13:44:35 +0200

    Refactored mkdir to use pattern matching

--- src/main/scala/filesystem/commands/MkDir.scala
+++ src/main/scala/filesystem/commands/MkDir.scala
@@ -5,17 +5,21 @@ import filesystem.{Dir, State}
 object MkDir extends Command {
   override val NAME = "mkdir"
 
-  override def apply(state: State = State(), args: List[String] = List()): State =
-    if (args.length != 1) state.out("usage: mkdir directory")
-    else {
-      val directory = args.head
-      if (state.cwd.hasEntry(directory)) state.out(s"mkdir: ${directory}: File exists")
-      else {
-        val newRoot = state.root.addEntry(Dir(directory), state.cwd.absolutePath)
 
-        state
-          .changeRoot(newRoot)
-          .cleanOut()
-      }
-    }
+  override def apply(state: State = State(), args: List[String] = List()): State = args match {
+    case List(directory) =>
+      if (state.cwd.hasEntry(directory))
+        state.out(s"mkdir: ${directory}: File exists")
+      else
+        mkdir(state, directory)
+    case _ => state.out("usage: mkdir directory")
+  }
+
+  private def mkdir(state: State, directory: String): State = {
+    val newRoot = state.root.addEntry(Dir(directory), state.cwd.absolutePath)
+
+    state
+      .changeRoot(newRoot)
+      .cleanOut()
+  }
 }
