0588cc90bc5c43106f829f1de9b0e510d7d678df
Oriol Barcelona <obarcelonap@gmail.com>
2020-05-08 13:56:27 +0200

    Refactored touch to use pattern matching

--- src/main/scala/filesystem/commands/Touch.scala
+++ src/main/scala/filesystem/commands/Touch.scala
@@ -5,17 +5,20 @@ import filesystem.{Dir, File, State}
 object Touch extends Command {
   override val NAME: String = "touch"
 
-  override def apply(state: State, args: List[String]): State =
-    if (args.isEmpty) state.out("usage: touch filename")
-    else {
-      val filename = args.head
-      if (state.cwd.hasEntry(filename)) state.out(s"touch: ${filename}: File exists")
-      else {
-        val newRoot = state.root.addEntry(File(filename), state.cwd.absolutePath)
+  override def apply(state: State = State(), args: List[String] = List()): State = args match {
+    case List(filename) =>
+      if (state.cwd.hasEntry(filename))
+        state.out(s"touch: ${filename}: File exists")
+      else
+        touch(state, filename)
+    case _ => state.out("usage: touch filename")
+  }
 
-        state
-          .changeRoot(newRoot)
-          .cleanOut()
-      }
-    }
+  private def touch(state: State, filename: String): State = {
+    val newRoot = state.root.addEntry(File(filename), state.cwd.absolutePath)
+
+    state
+      .changeRoot(newRoot)
+      .cleanOut()
+  }
 }
